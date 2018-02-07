fc010998def421a0621b9a385c7cfe2e29c37003
hferg <henry.fergusongow@gmail.com>
2018-02-07 19:00:53 +0000

    added print function that contains some summary information about a posterior

--- R/BTprocessR_S3.R
+++ R/BTprocessR_S3.R
@@ -1,25 +1,48 @@
+#' A print method for class bt_post
+#' Displays some descriptive info about the posterior in a table
+#'
+#' @name print.bt_post
+
+print.bt_post <- function(x) {
+  z <- x
+  class(z) <- class(x)[-1]
+  params <- colnames(x)
+  params <- params[!params %in% c("Iteration", "Tree.No")]
+  st <- matrix(nrow = length(params), ncol = 5)
+  colnames(st) <- c("Parameter", "Median", "Mean", "Mode", "SD")
+  st[ , 1] <- params
+  st[ , 2] <- round(apply(x[, params], 2, mean), 3)
+  st[ , 3] <- round(apply(x[, params], 2, median), 3)
+  st[ , 4] <- round(apply(x[, params], 2, modeStat), 3)
+  st[ , 5] <- round(apply(x[, params], 2, sd), 3)
+  cat("Posterior of ", nrow(x), " iterations\n\n")
+  print(data.frame(st))
+  cat("\n")
+  print(z)
+}
+
 #' An autoplot (ggplot2) method for objects of class "bt_post".
 #' Plots a histogram of all, or a specified number of, parameters
 #' from the posterior.
-#' 
+#'
 #' @param posterior An object of class "bt_post", returned from
 #' loadPosterior.
-#' @param parameter The parameter, or vector of parameter names, to plot. 
-#' Must match column names of posterior. If NULL then all parameters 
+#' @param parameter The parameter, or vector of parameter names, to plot.
+#' Must match column names of posterior. If NULL then all parameters
 #' will be plotted.
-#' @param col The colour of the bars in the histogram, or 
-#' a vector of colours (for multiple plots). If absent the 
+#' @param col The colour of the bars in the histogram, or
+#' a vector of colours (for multiple plots). If absent the
 #' viridis colour scale is used.
 #' @param pal If using the viridis pallettes (i.e. no user-specified
 #' colours) then this determines the viridis palletter used.
-#' See \link[viridis]{scale_color_viridis}. 
-#' Four options are available: "magma" (or "A"), "inferno" (or "B"), 
+#' See \link[viridis]{scale_color_viridis}.
+#' Four options are available: "magma" (or "A"), "inferno" (or "B"),
 #' "plasma" (or "C"), and "viridis" (or "D", the default option).
 #' @name autoplot.bt_post
 
-autoplot.bt_post <- function(posterior, parameters = NULL, col = NULL, 
+autoplot.bt_post <- function(posterior, parameters = NULL, col = NULL,
     pal = "D") {
-  d <- posterior[, !colnames(posterior) %in% c("Iteration", "Harmonic.Mean", "Tree.No")]  
+  d <- posterior[, !colnames(posterior) %in% c("Iteration", "Harmonic.Mean", "Tree.No")]
   if (!is.null(parameters)) {
     d <- d[, colnames(d) %in% parameters]
   } else {
@@ -28,7 +51,7 @@ autoplot.bt_post <- function(posterior, parameters = NULL, col = NULL,
   if (is.null(col)) {
     col <- viridis::viridis(length(parameters))
   }
-  
+
   d <- reshape2::melt(d)
   p <- ggplot2::ggplot(d, ggplot2::aes(value, fill = variable)) +
     ggplot2::geom_histogram() +
@@ -41,7 +64,7 @@ autoplot.bt_post <- function(posterior, parameters = NULL, col = NULL,
   p
 }
 
-#' A plot method for the class "bt_post" that invokes the 
+#' A plot method for the class "bt_post" that invokes the
 #' autoplot method.
 
 plot.bt_post <- function(x, ...) {
