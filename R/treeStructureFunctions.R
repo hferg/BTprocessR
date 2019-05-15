cba6fec1e4379a72e59a2799fd40716c997c3ea2
henry <henry.ferguson.gow@gmail.com>
2019-05-15 17:41:01 +0100

    Added a function to find shared branches between a tree and a smaller tree that is a subset of that first tree.

--- R/treeStructureFunctions.R
+++ R/treeStructureFunctions.R
@@ -146,3 +146,115 @@ getMRCAbtr <- function(x, tree, rjtaxa) {
   return(mrca)
 }
 
+################################################################################
+#' sharedBranches
+#'
+#' Make species key from a tree.
+#' @param tree An object of class "phylo"
+#' @name makeKey
+#' @keyword internal
+
+makeKey <- function(tree) {
+  tb <- matrix(ncol = 2, nrow = (nrow(tree$edge) + 1))
+  colnames(tb) <- c("ancNode", "descNode")
+  key <- vector(mode = "list", length = nrow(tb))
+  names(key) <- paste0("node_", c(0:nrow(tree$edge)))
+  tb[ , "ancNode"] <- c(0, tree$edge[ , 1])
+  tb[ , "descNode"] <- c((length(tree$tip.label) + 1), 
+    tree$edge[ , 2])
+
+  for (i in 2:nrow(tb)) {
+    descs <- phytools::getDescendants(tree, 
+      node = as.numeric(tb[i, "descNode"])
+    )
+    if (as.numeric(tb[i, "descNode"]) <= length(tree$tip.label)) {
+      sp <- tree$tip.label[descs]
+    } else {
+      tips <- descs[descs <= length(tree$tip.label)]
+      tips <- tree$tip.label[tips]
+      sp <- paste0(sort(tips), collapse = ",")
+    }
+    key[[i]]$nodes <- tb[i, 1:2]
+    key[[i]]$species <- strsplit(sp, ",")[[1]]
+  }
+  key[[1]] <- list(nodes = "root", species = tree$tip.label)
+  return(key)
+}
+
+################################################################################
+#' sharedBranches
+#'
+#' Finds shared branches between two trees where the smaller tree is a subset of
+#' the larger tree.
+#' @param tree1 An object of class "phylo"
+#' @param tree2 An object of class "phylo"
+#' @name sharedBranches
+#' @export
+
+sharedBranches <- function(tree1, tree2) {
+  # find the biggest tree.
+  if (tree1$Nnode > tree2$Nnode) {
+    big <- tree1
+    small <- tree2
+  } else if (tree1$Nnode < tree2$Nnode) {
+    big <- tree2
+    small <- tree1
+  } else if (tree1$Nnode == tree2$Nnode) {
+    stop("Both trees have the same number of nodes.")
+  }
+
+  # make sure that the smaller tree is a subset of the larger.
+  if (any(!small$tip.label %in% big$tip.label)) {
+    stop("Smaller tree is not a subset of larger tree.")
+  }
+  
+  bns <- big$tip.label[!big$tip.label %in% small$tip.label]
+  big_key <- makeKey(big)
+  small_key <- makeKey(small)
+  shared_table <- matrix(nrow = length(shared), ncol = 7)
+  colnames(shared_table) <- c("lg_nodeid", "lg_ancNode", "lg_descNode", 
+    "shared",
+    "sml_nodeid", "sml_ancNode", "sml_descNode")
+  for (i in seq_along(big_key)) {
+    shared[[i]]$nodes <- big_key[[i]]$nodes
+    sp <- big_key[[i]]$species
+    sp <- sp[!sp %in% bns]
+    big_key[[i]]$small_sp <- sp
+    if (i == 1) {
+      shared_table[i, "lg_nodeid"] <- "root"
+      shared_table[i, "lg_ancNode"] <- big_key[[i]]$nodes[1]
+      shared_table[i, "lg_descNode"] <- big_key[[i]]$nodes[1]
+    } else {
+      shared_table[i, "lg_nodeid"] <- names(big_key)[i]
+      shared_table[i, "lg_ancNode"] <- big_key[[i]]$nodes["ancNode"]
+      shared_table[i, "lg_descNode"] <- big_key[[i]]$nodes["descNode"]
+    }
+      
+    if (length(sp) != 0) {
+      shared_table[i, "shared"] <- TRUE
+      b_sp <- paste(sort(big_key[[i]]$small_sp), collapse = " ")
+      # now put in the anc/dec nodes of the smaller tree for the shared branch.
+      # find the branch from small_key that has matching species.
+      sh <- sapply(small_key, function(x) {
+        paste(sort(x$species), collapse = " ") == b_sp
+      })
+      sml_match <- small_key[[which(sh)]]
+      shared_table[i, "sml_nodeid"] <- names(which(sh))
+      if (i == 1) {
+        shared_table[i, "sml_ancNode"] <- "root"
+        shared_table[i, "sml_descNode"] <- "root"
+      } else {
+        shared_table[i, "sml_ancNode"] <- sml_match$nodes["ancNode"]
+        shared_table[i, "sml_descNode"] <- sml_match$nodes["descNode"]
+      }
+        
+    } else {
+      shared_table[i, "shared"] <- FALSE
+      shared_table[i, "sml_ancNode"] <- NA
+      shared_table[i, "sml_descNode"] <- NA
+    }
+  }
+  shared_table <- as.data.frame(shared_table)
+  shared$shared <- as.logical(shared)
+  return(tibble::as_tibble(shared_table))
+}
